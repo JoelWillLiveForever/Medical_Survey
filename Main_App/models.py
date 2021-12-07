7e54b548f5625dedb89c074fdb09c1fd329c5567
Vladimir Nikulin <mail.jorey@gmail.com>
2021-12-07 19:48:53 +0300

    Added question model prototype

--- Main_App/models.py
+++ Main_App/models.py
@@ -1,3 +1,7 @@
+from enum import Enum
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    name = models.TextField()
+    # answers_count = models.PositiveSmallIntegerField()
