ffc864a3445b91a37de9f99cf28a43149772db59
イゴーリ <f55dota217@gmail.com>
2022-01-26 17:57:17 +0300

    Cross-commit

--- Main_App/views.py
+++ Main_App/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_text
+
 from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout
+
 from .forms import RegistrationForm
+from .models import *
 
 # Create your views here.
 
@@ -15,7 +27,31 @@ def hello(request):
     return render(request, 'hello.html', context)
 
 def registration(request):
+    if request.user.is_authenticated:
+        pass
+        # return redirect('.html')
+    elif request.method == 'POST' and 'next_button' in request.POST:    
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # current_site = get_current_site(request)
+            # message = render_to_string('Main_App/acc_active_email.html', {
+            #     'user': user, 'domain': current_site.domain,
+            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token': account_activation_token.make_token(user),
+            # })
 
+            # # Sending activation link in terminal
+            # mail_subject = 'СОП. Активация аккаунта.'
+            # to_email = form.cleaned_data.get('email')
+            # email = EmailMessage(mail_subject, message, to=[to_email])
+            # email.send()
+            context= {
+                'regForm': form,
+            }
+        return render(request, 'registration.html', context)
+    elif request.method == 'POST' and 'back_button' in request.POST:
+        return redirect(request, 'hello.html')
     form = RegistrationForm()
     context= {
         'regForm': form,
