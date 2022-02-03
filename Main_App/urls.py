24521a3d7c51fed5345ee03227d3aca33024dd25
f55 <f55dota217@gmail.com>
2022-02-04 01:32:20 +0300

    Activation account by email

--- Main_App/urls.py
+++ Main_App/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.hello, name='hello'),
     path('registration', views.registration, name='registration'),
+    path('acc_active_sent', views.acc_active_sent, name='acc_active_sent'),
+    path('acc_active_confirmed', views.acc_active_confirmed, name='acc_active_confirmed'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
     path('questionA', views.questionA, name='questionA'),
 ]
--- Main_App/views.py
+++ Main_App/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 
+from .tokens import account_activation_token
 from django.core.mail import EmailMessage
 
 from django.shortcuts import redirect, render
@@ -31,20 +33,20 @@ def registration(request):
     if request.method == 'POST' and 'next_button':    
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # current_site = get_current_site(request)
-            # message = render_to_string('Main_App/acc_active_email.html', {
-            #     'user': user, 'domain': current_site.domain,
-            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            #     'token': account_activation_token.make_token(user),
-            # })
+            user = form.save()
+            current_site = get_current_site(request)
+            message = render_to_string('acc_active_email.html', {
+                'user': user, 'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
 
-            # # Sending activation link in terminal
-            # mail_subject = 'СОП. Активация аккаунта.'
-            # to_email = form.cleaned_data.get('email')
-            # email = EmailMessage(mail_subject, message, to=[to_email])
-            # email.send()
-            return redirect('questionA')
+            # Sending activation link in terminal
+            mail_subject = 'Медицинский опрос. Активация аккаунта.'
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+            return redirect('acc_active_sent')
         context= {
             'regForm': form,
         }
@@ -56,6 +58,32 @@ def registration(request):
         'regForm': form,
     }
     return render(request, 'registration.html', context)
+
+# активация акков
+def acc_active_sent(request):
+    return render(request, 'acc_active_sent.html')
+
+def acc_active_confirmed(request):
+    if request.method == 'POST' and 'back_button' in request.POST:
+        return redirect('questionA')
+    return render(request, 'acc_active_confirmed.html')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = get_user_model().objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
+        user = None
+
+    print(user)
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('acc_active_confirmed')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
 
 def questionA(request):
     context= {
